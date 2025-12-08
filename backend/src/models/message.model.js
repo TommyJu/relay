de459eca7549bd37f5e01a3c238db3bb4e9a4fa7
Tommy Ju <tommyju25@gmail.com>
2025-12-08 14:59:53 -0800

    Create schema for chat messages

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+
+// Defines the schema for chat messages.
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
