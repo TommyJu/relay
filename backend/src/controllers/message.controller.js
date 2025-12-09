290016d0fdb9e8e314a351d074b4b4d0dcd15e5e
Tommy Ju <tommyju25@gmail.com>
2025-12-08 16:35:59 -0800

    Implement controller function for sending messages

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 // Retrieves all users except for the user that is currently logged in.
 export const getUsersForSidebar = async (req, res) => {
@@ -13,6 +14,7 @@ export const getUsersForSidebar = async (req, res) => {
         res.status(500).json({ message: "Internal server error." });
     }
 };
+
 
 // Gets messages between the current user and message recipient.
 export const getMessages = async (req, res) => {
@@ -38,6 +40,39 @@ export const getMessages = async (req, res) => {
 
     } catch (error) {
         console.error("Error in message controller get messages function.", error.message);
+        res.status(500).json({ message: "Internal server error." });
+    }
+};
+
+
+// Sends a message to another user by creating a new message in the database.
+export const sendMessage = async (req, res) => {
+    try {
+        // Retrieves data from the request
+        const { text, image } = req.body;
+        const {recipientId} = req.params;
+        const currentUserId = req.user._id;
+
+        // Uploads an optional image to Cloudinary.
+        let imageUrl = "";
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        // Creates a new message to be sent.
+        const newMessage = new Message({
+            currentUserId,
+            recipientId,
+            text,
+            image: imageUrl
+        });
+
+        await newMessage.save();
+        res(201).json(newMessage);
+
+    } catch (error) {
+        console.error("Error in message controller send message function.", error.message);
         res.status(500).json({ message: "Internal server error." });
     }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar, getMessages } from "../controllers/message.controller.js";
+import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/message.controller.js";
 
 // Defines routes for user messaging
 const router = express.Router();
@@ -10,5 +10,8 @@ router.get("/users", protectRoute, getUsersForSidebar);
 
 // Defines a route for getting messages between the current user and recipient
 router.get("/:recipientId", protectRoute, getMessages);
+
+// Defines a route for sending a message to another user.
+router.post("/send/:recipientId", protectRoute, sendMessage);
 
 export default router;
