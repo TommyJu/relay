67113d51ca4f65a63b9ae742b7f7e25312e83e1b
Tommy Ju <tommyju25@gmail.com>
2025-12-08 16:18:15 -0800

    Implement logic for getting messages between current user and message recipient

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,14 +1,43 @@
-import User from "../models/user.model"
+import User from "../models/user.model.js"
+import Message from "../models/message.model.js";
 
+// Retrieves all users except for the user that is currently logged in.
 export const getUsersForSidebar = async (req, res) => {
     try {
-        // Retrieves all users except for the user that is currently logged in.
         const loggedInUserId = req.user._id;
         const otherUsers = await User.find({ _id: { $ne:loggedInUserId } }).select("-password");
 
         res.status(200).json(otherUsers);
     } catch (error) {
         console.error("Error in message controller get users for sidebar function.", error.message);
+        res.status(500).json({ message: "Internal server error." });
+    }
+};
+
+// Gets messages between the current user and message recipient.
+export const getMessages = async (req, res) => {
+    try {
+        const { recipientId } = req.params;
+        const currentUserId = req.user._id;
+        
+        // Retrieves both incoming and outgoing messages.
+        const messages = await Message.find({ 
+            $or: [
+                {
+                    senderId: senderId,
+                    receiverId: userToChatId
+                },
+                {
+                    receiverId: userToChatId,
+                    senderId: senderId
+                }
+            ]
+        });
+
+        res.status(200).json(messages);
+
+    } catch (error) {
+        console.error("Error in message controller get messages function.", error.message);
         res.status(500).json({ message: "Internal server error." });
     }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar } from "../controllers/message.controller.js";
+import { getUsersForSidebar, getMessages } from "../controllers/message.controller.js";
 
+// Defines routes for user messaging
 const router = express.Router();
 
+// Defines a route for getting users for the sidebar menu.
 router.get("/users", protectRoute, getUsersForSidebar);
+
+// Defines a route for getting messages between the current user and recipient
+router.get("/:recipientId", protectRoute, getMessages);
 
 export default router;
