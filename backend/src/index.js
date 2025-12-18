ca79767fcf77bfdc262c323a894ac11b1d84c812
Tommy Ju <tommyju25@gmail.com>
2025-12-17 22:49:42 -0800

    Setup backend to use socket.io

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,9 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 
-const app = express();
 app.use(express.json());
 app.use(cors(
     {
@@ -25,7 +25,7 @@ app.use("/api/messages", messageRoutes);
 
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
     connectDB();
 });
