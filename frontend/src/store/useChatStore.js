50e6e30f4c99c9d8dd09c3ff30f7bcf8547b7e4e
Tommy Ju <tommyju25@gmail.com>
2025-12-17 22:13:46 -0800

    Create send message function

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
     messages: [],
     users: [],
     selectedUser: null,
@@ -33,6 +33,16 @@ export const useChatStore = create((set) => ({
             toast.error(error.response.data.message);
         } finally {
             set({isMessagesLoading: false});
+        }
+    },
+
+    sendMessage: async (messageData) => {
+        const { selectedUser, messages } = get();
+        try {
+            const response = axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+            set({ messages: [...messages, response.data] });
+        } catch (error) {
+            toast.error(error.response.data.message);
         }
     },
 
