74cf3b8343448b1f3a147e75bf3cb6c010fb0134
Tommy Ju <tommyju25@gmail.com>
2025-12-20 20:16:06 -0800

    Clean up code for axios instance creation

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-
+import { API_BASE_URL } from "../config/api";
 
 // Creates an Axios instance for sending requests to the backend server.
 export const axiosInstance = axios.create( {
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" :  "/api",
+    baseURL: API_BASE_URL,
     withCredentials: true,
 } );
