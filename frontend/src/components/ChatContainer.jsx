6543d75e2a7b39c7412c603085a74f8a0711e588
Tommy Ju <tommyju25@gmail.com>
2025-12-25 15:43:24 -0800

    Create a NoMessages component that displays if the conversation is empty

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -6,6 +6,7 @@ import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
+import NoMessages from "./NoMessages";
 
 const ChatContainer = () => {
   const {
@@ -50,47 +51,50 @@ const ChatContainer = () => {
   return (
     <div className="flex flex-col h-full w-full px-2">
       <ChatHeader />
-
-      {/* messages container */}
-      <div className="flex-1 p-2 space-y-2 overflow-y-scroll overflow-x-hidden">
-        {messages.map((message) => (
-          <div
-            key={message._id}
-            className={`chat min-w-0 ${
-              message.senderId === authUser._id ? "chat-end" : "chat-start"
-            }`}
-            ref={messageEndRef}
-          >
-            <div className=" chat-image avatar">
-              <div className="size-10 rounded-full border">
-                <img
-                  src={
-                    message.senderId === authUser._id
-                      ? authUser.profilePic || "/avatar.png"
-                      : selectedUser.profilePic || "/avatar.png"
-                  }
-                  alt="profile pic"
-                />
+      {messages.length === 0 ? (
+        <NoMessages />
+      ) : (
+        <div className="flex-1 p-2 space-y-2 overflow-y-scroll overflow-x-hidden">
+          {messages.map((message) => (
+            <div
+              key={message._id}
+              className={`chat min-w-0 ${
+                message.senderId === authUser._id ? "chat-end" : "chat-start"
+              }`}
+              ref={messageEndRef}
+            >
+              <div className=" chat-image avatar">
+                <div className="size-10 rounded-full border">
+                  <img
+                    src={
+                      message.senderId === authUser._id
+                        ? authUser.profilePic || "/avatar.png"
+                        : selectedUser.profilePic || "/avatar.png"
+                    }
+                    alt="profile pic"
+                  />
+                </div>
+              </div>
+              <div className="chat-header mb-1">
+                <time className="text-xs opacity-50 ml-1">
+                  {formatMessageTime(message.createdAt)}
+                </time>
+              </div>
+              <div className="chat-bubble wrap-anywhere whitespace-pre-wrap">
+                {message.image && (
+                  <img
+                    src={message.image}
+                    alt="Attachment"
+                    className="rounded-md mb-2 w-full max-w-[90vw] sm:max-w-[200px]"
+                  />
+                )}
+                {message.text && <p>{message.text}</p>}
               </div>
             </div>
-            <div className="chat-header mb-1">
-              <time className="text-xs opacity-50 ml-1">
-                {formatMessageTime(message.createdAt)}
-              </time>
-            </div>
-            <div className="chat-bubble wrap-anywhere whitespace-pre-wrap">
-              {message.image && (
-                <img
-                  src={message.image}
-                  alt="Attachment"
-                  className="rounded-md mb-2 w-full max-w-[90vw] sm:max-w-[200px]"
-                />
-              )}
-              {message.text && <p>{message.text}</p>}
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
+
       <MessageInput />
     </div>
   );
