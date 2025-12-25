e26893e451e35d634c36948cf72fde0a55520caf
Tommy Ju <tommyju25@gmail.com>
2025-12-24 18:07:49 -0800

    Fix online user count

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -37,7 +37,8 @@ const Sidebar = () => {
             />
             <span className="text-sm">Show online only</span>
           </label>
-          <span className="text-xs text-zinc-500">({onlineUsers.length} online)</span>
+          {/* Excludes current user from online users count */}
+          <span className="text-xs text-zinc-500">({ onlineUsers.length - 1 } online)</span>
         </div>
       </div>
 
